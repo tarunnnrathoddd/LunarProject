aac4b88a5845e5b9314c63953d910258cab1631d
Prajakta514 <tambeprajakta10@gmail.com>
2025-03-05 21:46:10 +0530

    Added moon texture

---
+++ models/video_generator.py
@@ -0,0 +1,8 @@
+class VideoGenerator:
+    def __init__(self):
+        # Initialization code
+        pass
+
+    def generate_video(self, input_data):
+        # Video generation code
+        pass
